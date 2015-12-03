d90fa6af875a8ada04fb710179bf09a9851ab75a
zl3000 <u3523833.hku.hk>
2015-12-03 18:22:51 +0800

    finish basic function of class MyUser

--- app/src/main/java/hku/project/MySystem.java
+++ app/src/main/java/hku/project/MySystem.java
@@ -4,4 +4,7 @@ package hku.project;
  * Created by LZ on 15/12/3.
  */
 public class MySystem {
+
+    //uid need to be signed by the system, so need to check database to get the uid value
+
 }
--- app/src/main/java/hku/project/MyUser.java
+++ app/src/main/java/hku/project/MyUser.java
@@ -24,7 +24,121 @@ public class MyUser {
 
 
     public MyUser(){
-        
+        uid = 0;
+        name = "";
+        gender = 0;
+        imgPath = "";
+        title = 0;
+        sleepTime = new MyTime();
+        studyTime = new MyTime();
+        targetFinished = 0;
+        targetNotFinished = 0;
+        myRank = new MyRank();
+        myDress = new MyDress();
+        crtMission = new MyMission();
     }
 
+    public int getUid(){
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTitle() {
+        return title;
+    }
+
+    public void setTitle(int title) {
+        this.title = title;
+    }
+
+    public MyTime getSleepTime() {
+        return sleepTime;
+    }
+
+    public void setSleepTime(MyTime sleepTime) {
+        this.sleepTime = sleepTime;
+    }
+
+    public MyTime getStudyTime() {
+        return studyTime;
+    }
+
+    public void setStudyTime(MyTime studyTime) {
+        this.studyTime = studyTime;
+    }
+
+    public int getTargetFinished() {
+        return targetFinished;
+    }
+
+    public void setTargetFinished(int targetFinished) {
+        this.targetFinished = targetFinished;
+    }
+
+    public int getTargetNotFinished() {
+        return targetNotFinished;
+    }
+
+    public void setTargetNotFinished(int targetNotFinished) {
+        this.targetNotFinished = targetNotFinished;
+    }
+
+    public List<MyTarget> getMyTargetList() {
+        return myTargetList;
+    }
+
+    public void setMyTargetList(List<MyTarget> myTargetList) {
+        this.myTargetList = myTargetList;
+    }
+
+    public List<MyStar> getMyStarList() {
+        return myStarList;
+    }
+
+    public void setMyStarList(List<MyStar> myStarList) {
+        this.myStarList = myStarList;
+    }
+
+    public MyRank getMyRank() {
+        return myRank;
+    }
+
+    public void setMyRank(MyRank myRank) {
+        this.myRank = myRank;
+    }
+
+    public MyDress getMyDress() {
+        return myDress;
+    }
+
+    public void setMyDress(MyDress myDress) {
+        this.myDress = myDress;
+    }
+
+    public List<MyPlan> getMyPlanList() {
+        return myPlanList;
+    }
+
+    public void setMyPlanList(List<MyPlan> myPlanList) {
+        this.myPlanList = myPlanList;
+    }
+
+    public MyMission getCrtMission() {
+        return crtMission;
+    }
+
+    public void setCrtMission(MyMission crtMission) {
+        this.crtMission = crtMission;
+    }
 }
